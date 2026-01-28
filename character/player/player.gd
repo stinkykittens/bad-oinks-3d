9c7df0a12067d78363b47976df26de38ff66a6fd
stinkykittens <pvt.elisa@gmail.com>
2026-01-28 18:07:44 +0100

    Add statemachine to player and make it look in the direction you are going

--- character/player/player.gd
+++ character/player/player.gd
@@ -1,32 +1,77 @@
 class_name Player
 extends CharacterBody3D
 
+enum States {
+	Idle,
+	Run,
+	Jump,
+	Fall,
+	Dash
+}
+
+
 @export var move_speed := 8.0
 @export var move_deadzone := 0.25
 @export var gravity_scale := 1.0
+@export var jump_gravity_scale := 0.8
 @export var jump_strenght := 8.0
+
+@export var rotaion_smooth_speed_grounded := 6
+@export var rotaion_smooth_speed_air := 3
+
+
+var state: States
+
+var _target_rotation: float
 
 
 func _ready() -> void:
-	pass # Replace with function body.
+	state = States.Idle
 
 
 func _physics_process(delta: float) -> void:
-	var input: Vector3
+	var input: Vector2
 	input.x = Input.get_axis("move_left", "move_right")
-	input.z = Input.get_axis("move_up", "move_down")
-	var input_scale = smoothstep(move_deadzone, 1, input.length())
+	input.y = Input.get_axis("move_up", "move_down")
+	var input_scale = min(1, input.length())
 	input = input.normalized() * input_scale
+	var camera := get_tree().root.get_camera_3d()
+	var input_direction := Vector3(input.x, 0, input.y).rotated(Vector3.UP, camera.global_rotation.y)
 	
-	var angle = get_tree().root.get_camera_3d().global_rotation.y
-	input = input.rotated(Vector3.UP, angle)
-	rotation.y = input.angle_to(Vector3.UP)
+	if is_on_floor() and (state == States.Fall or state == States.Jump):
+		# Ground entered
+		state = States.Idle
 	
-	velocity.x = input.x
-	velocity.z = input.z
-	velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * gravity_scale * delta
+	if not is_on_floor() and (state == States.Idle or state == States.Run):
+		# Ground exited
+		state = States.Fall
+	
+	if state == States.Idle:
+		velocity.x = 0
+		velocity.z = 0
+		velocity.y = -1
+		if input_scale > move_deadzone:
+			state = States.Run
+	
+	if state == States.Run:
+		velocity.y = -1
+		velocity.x = input_direction.x
+		velocity.z = input_direction.z
+		if input_scale < move_deadzone:
+			state = States.Idle
+		else:
+			_target_rotation = camera.global_rotation.y + input.angle_to(Vector2.UP)
+	
+	if state == States.Fall:
+		velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * gravity_scale * delta
+	
+	if state == States.Jump:
+		velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * jump_gravity_scale * delta
+	
+	rotation.y = lerp_angle(rotation.y, _target_rotation, delta * (rotaion_smooth_speed_grounded if is_on_floor() else rotaion_smooth_speed_air))
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_strenght
 	
 	move_and_slide()
+	print(state)
