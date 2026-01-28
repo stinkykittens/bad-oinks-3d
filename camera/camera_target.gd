06cb7ca2603dc0e536b37d3b794b72a0f2768a57
stinkykittens <pvt.elisa@gmail.com>
2026-01-28 22:02:59 +0100

    Fix camera jitter

--- camera/camera_target.gd
+++ camera/camera_target.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var _start_active: bool
 @export_range(0, 10) var influence := 1.0
 @export var interpolate := false
+@export var physics_frame := false
 @export_range(0, 5) var interpolation_speed := 1.0
 @export var smooth_activation := true
 @export var smooth_activation_time := 1.0
@@ -27,15 +28,20 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if disabled():
-		return
-	
+	if not disabled() and not physics_frame:
+		_update_position(delta)
+
+
+func _physics_process(delta: float) -> void:
+	if not disabled() and physics_frame:
+		_update_position(delta)
+
+
+func _update_position(delta: float) -> void:
 	var target_position = get_parent().global_position + _offset
-	
 	if not interpolate:
 		position = target_position
 		return
-	
 	position = position.lerp(target_position, delta * interpolation_speed)
 
 
